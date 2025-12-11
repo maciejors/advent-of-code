b89fcb8104a64d26b2d57c12f561cb6480564f7a
maciejors <maciej.orslowski@gmail.com>
2025-12-11 12:01:39 +0100

    [2024] enhance prep_day script

--- aoc2024/src/bin/prep_day.rs
+++ aoc2024/src/bin/prep_day.rs
@@ -1,6 +1,6 @@
 use std::{env, fs, io};
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
@@ -10,11 +10,30 @@ fn main() -> Result<(), io::Error> {
     let boilerplate_code = fs::read_to_string("./data/dayXX.rs.txt")?
         .replace("XX", day_id);
 
-    fs::write(format!("./src/bin/day{}.rs", day_id), boilerplate_code)?;
+    let rs_file_path = format!("./src/bin/day{}.rs", day_id);
+    create_file(rs_file_path, Some(boilerplate_code));
 
     let data_dir = PathBuf::from(format!("./data/day{}", day_id));
     fs::create_dir_all(&data_dir)?;
-    File::create(&data_dir.join("example.txt"))?;
-    File::create(&data_dir.join("input.txt"))?;
+    create_file(&data_dir.join("example.txt"), None);
+    create_file(&data_dir.join("input.txt"), None);
+
     Ok(())
+}
+
+fn create_file<P: AsRef<Path>>(path: P, content: Option<String>) {
+    let path = path.as_ref();
+    if path.exists() {
+        println!("- Already exists: `{}`", path.display());
+        return;
+    }
+    match File::create(path) {
+        Ok(_) => {
+            if let Some(content) = content {
+                fs::write(path, content).unwrap();
+            }
+            println!("✔ Created `{}`", path.display());
+        },
+        Err(_) => println!("✖ Failed to create `{}`", path.display()),
+    }
 }
